d6ab497db161377bfb9287e325186c310aebc6cc
EC2 Default User <ec2-user@ip-172-31-46-197.ec2.internal>
2021-04-22 07:56:41 +0000

    [Add]seedファイルに初期データの記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ List.create!(
+    title: 'railsを学ぼう',
+    body: 'seedファイルについて'
+  )
+
+    10.times do |number|
+    List.create!(title: 'timesを学習しよう',body: number)
+  end
